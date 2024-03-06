6f8ff68d2a23fd56e7a0e898a55e33b5c85db03d
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-03-06 19:53:20 +0000

    Submissão 3

--- src/main/java/Semana3/ResizingArrayQueueOfStrings.java
+++ src/main/java/Semana3/ResizingArrayQueueOfStrings.java
@@ -10,6 +10,25 @@ public class ResizingArrayQueueOfStrings {
     this.last = -1;
   }
 
+  public String toString() {
+    String result = "";
+
+    for(int i = 0; i < this.queue.length; i++) {
+      result += this.queue[i % this.queue.length] + " | ";
+    }
+
+    return result;
+  }
+
+  public void shift() {
+    String lastItem = this.queue[last];
+    resize(this.queue.length);
+
+    for(int i = this.last; i > this.first; i--)
+      this.queue[i] = this.queue[i - 1];
+    this.queue[0] = lastItem;
+  }
+
   private int size() {
     if(isEmpty()) return 0;
 
@@ -28,6 +47,8 @@ public class ResizingArrayQueueOfStrings {
       resize(2 * this.queue.length);
 
     this.last = next(this.last);
+    // Funciona porque o criterio de avaliação de vazio é first = -1; Se mudar lá, tem que mudar aqui
+    if (isEmpty()) this.first = 0;
     this.queue[this.last] = item;
   }
 
@@ -36,13 +57,16 @@ public class ResizingArrayQueueOfStrings {
       throw new IllegalStateException("Queue underflow!");
 
     String result = this.queue[this.first];
-    this.queue = null;
-    if(this.first == this.last) {
-      this.first = -1;
-      this.last = -1;
-    } else {
-      this.first = next(this.first);
-    }
+    this.queue[this.first] = null;
+
+    this.first = next(this.first);
+
+    if(this.queue[this.first] == null) this.first = -1;
+
+    if(isEmpty()) resize(1);
+    if(this.size() <= this.queue.length / 4)
+      resize(Math.max(this.queue.length / 2, 1));
+
     return result;
   }
 
@@ -53,9 +77,18 @@ public class ResizingArrayQueueOfStrings {
   private void resize(int newSize) {
     String[] newA = new String[newSize];
 
-    for(int i = this.first; i < last + size(); i++)
+    for (int i = Math.max(this.first, 0); i < this.queue.length + this.size() - 1; i++) {
+      if(this.queue[i % this.queue.length] == null) break;
       newA[i - this.first] = this.queue[i % this.queue.length];
-
+    }
     this.queue = newA;
+
+    if(this.queue[0] == null) {
+      this.first = -1;
+      this.last = -1;
+    } else {
+      this.last = this.last - this.first;
+      this.first = 0;
+    }
   }
 }
