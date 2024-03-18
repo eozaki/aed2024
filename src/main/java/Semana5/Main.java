88d3c22548d381ad252751ca72d65e8e5c30e44f
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-03-18 17:56:29 +0000

    Add tests for removals

--- src/main/java/Semana5/Main.java
+++ src/main/java/Semana5/Main.java
@@ -1,7 +1,5 @@
 package Semana5;
 
-import Utils.Test;
-
 import static Utils.Test.assertEquals;
 
 public class Main {
@@ -13,34 +11,48 @@ public class Main {
     assertEquals(a.isEmpty(), false);
     a.add("b"); // a - b
     a.add("c"); // a - b - c
-    assertEquals(a.get(2), "c");
+    assertEquals(a.get(3), "c");
 
-    a.remove(1); // a - c
-    assertEquals(a.get(1), "c");
+    a.remove(2); // a - c
+    assertEquals(a.get(2), "c");
 
     a.add("a"); // a - c - a
     a.removeFirst("a"); // c - a
-    assertEquals(a.get(0), "c");
+    assertEquals(a.get(1), "c");
 
     a.add("c"); // c - a - c
     a.add("a"); // c - a - c - a
     assertEquals(a.size(), 4);
-    assertEquals(a.get(2), "c");
+    assertEquals(a.get(3), "c");
     a.removeLast("c"); // c - a - a
 
-    assertEquals(a.get(1), "a");
     assertEquals(a.get(2), "a");
+    assertEquals(a.get(3), "a");
     assertEquals(a.size(), 3);
     a.removeAll("a"); // c
     assertEquals(a.size(), 1);
-    assertEquals(a.get(0), "c");
+    assertEquals(a.get(1), "c");
 
     assertEquals(a.isPalindrome(), true);
     a.add("a"); // c - a
     assertEquals(a.isPalindrome(), false);
     a.add("c"); // c - a - c
     assertEquals(a.isPalindrome(), true);
-    while(!a.isEmpty()) a.remove(0); // -----EMPTY-----
+
+    assertEquals(a.removeFirst("j"), false);
+    assertEquals(a.size(), 3);
+    assertEquals(a.removeLast("j"), false);
+    assertEquals(a.size(), 3);
+    assertEquals(a.removeAll("j"), false);
+    assertEquals(a.size(), 3);
+
+    assertEquals(a.removeAll("a"), true);
+    assertEquals(a.size(), 2);
+
+    assertEquals(a.removeFirst("c"), true);
+    assertEquals(a.size(), 1);
+    assertEquals(a.removeLast("c"), true);
+    assertEquals(a.size(), 0);
 
     List<Character> c = new List<Character>();
     String b = "socorrammesubinoonibusemmarrocos";
@@ -50,7 +62,12 @@ public class Main {
 
     assertEquals(c.isPalindrome(), true);
 
-    c.remove(0);
+    assertEquals(c.removeAll('j'), false);
+    assertEquals(c.removeAll('o'), true);
+
+    assertEquals(c.isPalindrome(), true);
+
+    c.remove(1);
     assertEquals(c.isPalindrome(), false);
 
     for(Character s : c) System.out.println(s);
