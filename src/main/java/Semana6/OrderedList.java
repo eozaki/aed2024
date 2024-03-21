263878d1787528844e7418f4eff9909074dad9d6
Erick Cordeiro Ozaki <erick@MBP-de-Erick.Home>
2024-03-21 16:02:17 +0000

    Aula 6

---
+++ src/main/java/Semana6/OrderedList.java
@@ -0,0 +1,9 @@
+package Semana6;
+
+//public class OrderedList<T extends Comparable<T>> implements Iterable<T> {
+//
+//  public void sort();
+//  public boolean isSorted();
+//
+//  public void shuffle();
+//}
