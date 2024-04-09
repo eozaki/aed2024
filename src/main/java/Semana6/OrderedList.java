4602b32af109c8b5068a48f623593d4628069189
Erick Cordeiro Ozaki <erick@MBP-de-Erick.Home>
2024-04-09 23:40:07 +0100

    Submissão 6

--- src/main/java/Semana6/OrderedList.java
+++ src/main/java/Semana6/OrderedList.java
@@ -1,19 +1,22 @@
 package Semana6;
 
-import Semana5.List;
-
 import java.util.Iterator;
 import java.util.Objects;
 
-public class OrderedList<T> implements Iterable<T> {
+public class OrderedList<T extends Comparable<T>> implements Iterable<T> {
   public class Node {
     public T item;
-    public Node next, previous;
+    public Node next;
+    public Node previous;
 
     public Node(T item, Node next, Node previous) {
       this.item = item;
       this.next = next;
       this.previous = previous;
+    }
+
+    public boolean lessThan(Node other) {
+      return this.item.compareTo(other.item) < 0;
     }
   }
 
@@ -30,9 +33,9 @@ public class OrderedList<T> implements Iterable<T> {
     this.size = 0;
   }
 
-  private class OrderdListIterator implements Iterator<T> {
+  private class ListIterator implements Iterator<T> {
     private Node current;
-    public OrderdListIterator() {
+    public ListIterator() {
       this.current = head.next;
     }
 
@@ -47,7 +50,7 @@ public class OrderedList<T> implements Iterable<T> {
     }
   }
   public Iterator<T> iterator() {
-    return new OrderdListIterator();
+    return new ListIterator();
   }
 
   public boolean isEmpty() {
@@ -65,87 +68,6 @@ public class OrderedList<T> implements Iterable<T> {
     this.size++;
   }
 
-  public T get(int index) {
-    if(index > this.size || index < 1) throw new IllegalArgumentException("Stack overflow :'(");
-
-    Node current = head.next;
-    while(index > 1) {
-      current = current.next;
-      index--;
-    }
-
-    return current.item;
-  }
-
-  public T remove(int index) {
-    if(index > this.size || index < 1) throw new IllegalArgumentException("Stack overflow :'(");
-
-    Node current = head.next;
-    while(index > 1) {
-      current = current.next;
-      index--;
-    }
-    T t = current.item;
-
-    current.previous.next = current.next;
-    current.next.previous = current.previous;
-
-    this.size--;
-    return t;
-  }
-
-  public boolean removeFirst(T item) {
-    Node current = this.head.next;
-
-    while(current != this.tail) {
-      if(current.item == item) {
-        current.previous.next = current.next;
-        current.next.previous = current.previous;
-        current = null;
-
-        this.size--;
-        return true;
-      }
-
-      current = current.next;
-    }
-
-    return false;
-  }
-
-  public boolean removeLast(T item) {
-    Node current = this.tail.previous;
-    while(current != this.head) {
-      if(current.item == item) {
-        current.previous.next = current.next;
-        current.next.previous = current.previous;
-        current = null;
-
-        this.size--;
-        return true;
-      }
-
-      current = current.previous;
-    }
-
-    return false;
-  }
-
-  public boolean removeAll(T item) {
-    boolean removed = false;
-
-    for(Node current = head.next; current != tail; current = current.next)
-      if(Objects.equals(current.item, item)) {
-        current.previous.next = current.next;
-        current.next.previous = current.previous;
-        removed = true;
-
-        this.size--;
-      }
-
-    return removed;
-  }
-
   public boolean contains(T item) {
     for(Node current = this.head.next; current != this.tail; current = current.next) {
       if(Objects.equals(current.item, item)) {
@@ -156,20 +78,38 @@ public class OrderedList<T> implements Iterable<T> {
     return false;
   }
 
-  public boolean isPalindrome() {
-    Node upwards = head.next;
-    Node downwards = tail.previous;
+  public void sort() {
+    for(Node i = this.head.next; i != this.tail; i = i.next)
+      for(Node j = i.previous; j != this.head && j.next.lessThan(j); j = j.previous)
+        swap(j, j.next);
+  }
 
-    while(upwards != tail && downwards != head) {
-      if (!Objects.equals(upwards.item, downwards.item)) return false;
-      upwards = upwards.next;
-      downwards = downwards.previous;
-    }
+  private void swap(Node a, Node b) {
+    T temp = b.item;
+
+    b.item = a.item;
+    a.item = temp;
+  }
+
+  public boolean isSorted() {
+    for(Node i = this.tail.previous; i != this.head.next && i != this.head; i = i.previous)
+      if(i.lessThan(i.previous)) return false;
+
     return true;
   }
 
-  public void sort();
-  public boolean isSorted();
+  public String toString() {
+    String a = "";
 
-  public void shuffle();
+    for(Node i = this.head.next; i != this.tail; i = i.next)
+      a = a.concat(i.item.toString());
+
+    return a;
+  }
+
+  public void shuffle() {
+    for(Node i = this.head.next; i != this.tail; i = i.next)
+      for(Node j = i.previous; j != this.head && Math.random() < 0.5; j = j.previous)
+        swap(j, j.next);
+  }
 }
