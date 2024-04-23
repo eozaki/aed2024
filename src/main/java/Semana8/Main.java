ea51569ad76bff51ac313f9b7a7b679685a129e8
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-04-23 10:31:24 +0100

    tests some more

--- src/main/java/Semana8/Main.java
+++ src/main/java/Semana8/Main.java
@@ -1,10 +1,12 @@
 package Semana8;
 
+import java.util.Arrays;
+
 import static Utils.Test.assertEquals;
 
 public class Main {
   public static void main(String[] args) {
-    int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    Integer[] a = { 0, 1, 2, 3, 4, 5, 6 };
     assertEquals(HeapSorting.left(0), 1);
     assertEquals(HeapSorting.right(0), 2);
 
@@ -22,9 +24,17 @@ public class Main {
     assertEquals(HeapSorting.parent(5), 2);
     assertEquals(HeapSorting.parent(6), 2);
 
-    Integer[] b = {0, 1, 2, 3, 4, 5, 6};
-    HeapSorting.sink(b, 0, 7);
-    assertEquals(b[0], 5);
-    assertEquals(b[6], 0);
+    HeapSorting.sink(a, 0, 7);
+
+    assertEquals(a[0], 2);
+    assertEquals(a[1], 1);
+    assertEquals(a[2], 6);
+    assertEquals(a[3], 3);
+    assertEquals(a[4], 4);
+    assertEquals(a[5], 5);
+    assertEquals(a[6], 0);
+
+    for(int i = 0; i < a.length / 2; i++) HeapSorting.sink(a, i, 7);
+    System.out.println(Arrays.toString(a));
   }
 }
