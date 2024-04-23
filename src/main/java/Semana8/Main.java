2f6b775bf4866d692c1f44701107700f4b6bfad7
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2024-04-23 18:51:19 +0100

    Tests with tree visualization

--- src/main/java/Semana8/Main.java
+++ src/main/java/Semana8/Main.java
@@ -1,20 +1,24 @@
 package Semana8;
 
+import Utils.TreePrinter;
+
 import java.util.Arrays;
 
+import static Semana8.HeapSorting.left;
+import static Semana8.HeapSorting.right;
 import static Utils.Test.assertEquals;
 
 public class Main {
   public static void main(String[] args) {
     Integer[] a = { 0, 1, 2, 3, 4, 5, 6 };
-    assertEquals(HeapSorting.left(0), 1);
-    assertEquals(HeapSorting.right(0), 2);
+    assertEquals(left(0), 1);
+    assertEquals(right(0), 2);
 
-    assertEquals(HeapSorting.left(1), 3);
-    assertEquals(HeapSorting.right(1), 4);
+    assertEquals(left(1), 3);
+    assertEquals(right(1), 4);
 
-    assertEquals(HeapSorting.left(2), 5);
-    assertEquals(HeapSorting.right(2), 6);
+    assertEquals(left(2), 5);
+    assertEquals(right(2), 6);
 
     assertEquals(HeapSorting.parent(0), 0);
     assertEquals(HeapSorting.parent(1), 0);
@@ -31,14 +35,26 @@ public class Main {
     }
 
     HeapSorting.heapify(a, a.length);
-    expected = new int[]{6, 4, 5, 3, 1, 2, 0};
+    expected = new int[]{6, 4, 5, 1, 3, 2, 0};
     for(int i = 0; i < expected.length; i++) {
       assertEquals(a[i], expected[i]);
     }
 
-    Integer[] b = new Integer[15];
-    for(int i = 0; i < 15; i++) { b[i] = (int) (Math.random() * 100); }
+    Integer[] b = new Integer[31];
+    for(int i = 0; i < 31; i++) { b[i] = (int) (Math.random() * 100); }
     HeapSorting.heapify(b, b.length);
-    System.out.println(Arrays.toString(b));
+    print(b, b.length);
+
+    HeapSorting.sortdown(b, b.length);
+    print(b, b.length);
+  }
+
+  public static void print(Comparable[] arr, int N) {
+    TreePrinter<Integer> printer = new TreePrinter<>(k -> "" + arr[k],
+      k -> left(k) < N ? left(k) : null,
+      k -> right(k) < N ? right(k) : null);
+    printer.setHspace(2); //espaço horizontal entre nós
+    printer.setSquareBranches(true); //acho que gosto mais assim, mas false também é fixe
+    printer.printTree(0); //1 = root, isto é com a heap a começar no índice 1
   }
 }
